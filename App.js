e01d967b8706ce0b1043d299b0a3176aefc55d01
cyang0227 <cyang0227@gmail.com>
2024-04-13 18:34:09 -0400

    update

--- App.js
+++ App.js
@@ -7,32 +7,27 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
-import session from "express-session";
-import "dotenv/config";
+// import session from "express-session";
+// import "dotenv/config";
 
 const app = express();
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL, // URL of the React app that we're connecting to
-    credentials: true,
-  })
-);
-const sessionOptions = {
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-};
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.HTTP_SERVER_DOMAIN,
-  };
-}
-app.use(session(sessionOptions));
+app.use(cors());
+// const sessionOptions = {
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+// };
+// if (process.env.NODE_ENV !== "development") {
+//   sessionOptions.proxy = true;
+//   sessionOptions.cookie = {
+//     sameSite: "none",
+//     secure: true,
+//     domain: process.env.HTTP_SERVER_DOMAIN,
+//   };
+// }
+// app.use(session(sessionOptions));
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
